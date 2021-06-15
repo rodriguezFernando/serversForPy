3e4e1e10d987f14cb7e669778b51beecef35070c
rodriguezFernando <rodriguezcortes.fer@gmail.com>
2021-06-15 16:54:35 -0500

    new version multiple-client server

--- multipleSockets.py
+++ multipleSockets.py
@@ -2,17 +2,27 @@ import socket
 import select
 
 sockets = []
-
 for port in range(8000,8003):
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    server_socket.bind(('192.168.1.172', port))
+    server_socket.bind(('192.168.1.126', port))
     sockets.append(server_socket)
 
 empty = []
-while True:
-    readable, writable, exceptional = select.select(sockets, empty, empty)
-    for s in readable:
-         (client_data, client_address) = s.recvfrom(65536)
-         print(client_address, client_data)
-    for s in sockets:
-        s.close()
+try:
+	while True:
+	 readable, writable, exceptional = select.select(sockets, empty, empty)
+	 for s in readable:
+	  (client_data, client_address) = s.recvfrom(65536)
+	  print(client_address, client_data.decode('ascii'))
+
+except KeyboardInterrupt:
+	print("Exiting Program")
+
+except Exception as exception_error:
+	print("Error occurred. Exiting Program")
+	print("Error: " + str(exception_error))
+
+finally:
+	for s in sockets:
+	 s.close()
+	pass
