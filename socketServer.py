9ca554b1df318c9b0e620ed4e95fb7093d1485e9
rodriguezFernando <rodriguezcortes.fer@gmail.com>
2021-06-16 13:25:30 -0500

    STOP signal detection

--- socketServer.py
+++ socketServer.py
@@ -1,12 +1,18 @@
 import socket
+import re
 
-#
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-server_socket.bind(("192.168.100.19", 8080))
+server_socket.bind(('192.168.1.126', 8080))
+
+
 
 while True:
-       data, addr = server_socket.recvfrom(65536) 
-       text = data.decode('ascii')
-       print("received message {} from {}".format(text, addr))
-       
-       
+ patterns=['<1.8.4.1>', 'Boton de paro']
+ data, addr = server_socket.recvfrom(65536)
+ text = data.decode('ascii')
+ print("received message {} from {}".format(text, addr))
+ for pattern in patterns:
+	 if re.search(pattern, text):
+	  print("match found")
+	 else:
+	  print("not found")   
